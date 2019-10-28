80c2e9b290bef8a0d9097a8f0d78805fd69925ea
toa0904 <pazutomo0428@icloud.com>
2019-10-28 18:56:56 +0900

    変更しました

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,24 +1,26 @@
-$(function(){
-  function buildHTML(message){
-    var addImage =(message.image !== null) ? `<img src="${message.image}"` : '' ;
-    var html =
-       `<div class="message" data-message-id=${message.id}>
-          <div class="message__upper-info">
-            <div class="message__upper-info__talker">
+$(function () {
+
+  function buildHTML(message) {
+
+    image = (message.image) ? `<img class= "lower-message__image" src=${message.image} >` : ""; 
+
+    var html = `<div class="message" data-message-id="${message.id}"> 
+          <div class="upper-message">
+            <div class="upper-message__user-name">
               ${message.user_name}
             </div>
-            <div class="message__upper-info__date">
-              ${message.date}
+            <div class="upper-message__date">
+              ${message.created_at}
             </div>
           </div>
-          <div class="message">
-            <p class="message__text">
+          <div class="lower-meesage">
+            <p class="lower-message__content">
               ${message.content}
             </p>
+            ${image}
           </div>
-          ${addImage}
         </div>`
-      return html;
+    return html;
   }
 
   $('#new_message').on('submit', function(e){
@@ -44,4 +46,32 @@ $(function(){
     });
     return false;
   });
-});
+
+  var reloadMessages = function () {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $('.message:last').data("message-id");
+
+      $.ajax({ 
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json', 
+        data: {last_id: last_message_id} 
+      })
+      .done(function (messages) {
+        var insertHTML = '';
+        messages.forEach(function (message) {
+          insertHTML = buildHTML(message); 
+          $('.messages').append(insertHTML);
+        })
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      })
+      .fail(function () {
+        alert('自動更新に失敗しました');
+      });
+    }
+  };
+  setInterval(reloadMessages, 5000);
+  });
+
+
+// });
