252a99ba94ee919fdf4d99035cf882e81864dd4a
toa0904 <pazutomo0428@icloud.com>
2019-10-29 16:39:24 +0900

    AWS

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,6 +8,8 @@ pid "#{app_path}/tmp/pids/unicorn.pid"
 
 listen 3000
 
+listen "#{app_path}/tmp/sockets/unicorn.sock"
+
 stderr_path "#{app_path}/log/unicorn.stderr.log"
 
 stdout_path "#{app_path}/log/unicorn.stdout.log"
