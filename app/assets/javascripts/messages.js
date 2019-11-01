c8da7c73c312709e7cd024e13ec26a8c108f9171
toa0904 <pazutomo0428@icloud.com>
2019-11-01 20:07:37 +0900

    変更しました

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -62,6 +62,7 @@ $(function () {
         messages.forEach(function (message) {
           insertHTML = buildHTML(message); 
           $('.messages').append(insertHTML);
+          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
         })
       })
       .fail(function () {
